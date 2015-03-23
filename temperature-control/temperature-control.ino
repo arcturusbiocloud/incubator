8da75c488cd593304447e154a63cad19483d15d3
luisbebop <luisbebop@gmail.com>
2015-03-22 23:21:49 -0700

    updated the daemon that controls the incubator and the lights from the arc robot

--- temperature-control/temperature-control.ino
+++ temperature-control/temperature-control.ino
@@ -30,18 +30,16 @@ void setup()
     
     // serial handshake
     Serial.begin(9600);
-    Serial.println("Arcturus Incubator and lights controller connected");   
     // starts the temperature sensor
     dht.begin();
 }
 
 int UV_ON = 0;
+char buffer[64];
+size_t len = 0;
 
 void loop() 
-{
-    char buffer[64];
-    size_t length = 1;
-   
+{  
     // clean the serial buffer
     memset(buffer,0,sizeof(buffer));
    
@@ -52,8 +50,18 @@ void loop()
     
     // implement serial read bytes and read command
     if (Serial.available()) {
-      length = Serial.readBytes(buffer, length);
-      if (length > 0) {
+      len = Serial.readBytes(buffer, 1);
+      if (len > 0) {
+        if (buffer[0] == '0') {
+          UV_ON = 0;
+          //turn incubator light on
+          digitalWrite(INCUBATOR_LIGHT_PIN, LOW);
+          // turn tent light on
+          digitalWrite(TENT_LIGHT_PIN, LOW);
+          // turn the UV light off
+          digitalWrite(UV_PIN, LOW);
+          Serial.println("0");
+        }
         if (buffer[0] == '1') {
           UV_ON = 1;
           //turn incubator light off
@@ -64,32 +72,20 @@ void loop()
           digitalWrite(UV_PIN, HIGH);
           Serial.println("1");
         }
-        if (buffer[0] == '0') {
-          UV_ON = 0;
-          //turn incubator light on
-          digitalWrite(INCUBATOR_LIGHT_PIN, LOW);
-          // turn tent light on
-          digitalWrite(TENT_LIGHT_PIN, LOW);
-          // turn the UV light off
-          digitalWrite(UV_PIN, LOW);
-          Serial.println("0");
-         }
-         if (buffer[0] == '2') {
-         // check if returns are valid, if they are NaN (not a number) then something went wrong!
-           if (isnan(t) || isnan(h)) 
-           {
-             Serial.println("Failed to read from DHT");
-           } 
-           else 
-           {
-             Serial.print("Humidity: "); 
-             Serial.print(h);
-             Serial.print(" %\t");
-             Serial.print("Temperature: "); 
-             Serial.print(t);
-             Serial.println(" *C");
-           }      
-         }
+        if (buffer[0] == '2') {
+          // check if returns are valid, if they are NaN (not a number) then something went wrong!
+          if (isnan(t) || isnan(h)) 
+          {
+            Serial.println("Failed to read from DHT");
+          } else {
+            Serial.print("Humidity: "); 
+            Serial.print(h);
+            Serial.print(" %\t");
+            Serial.print("Temperature: "); 
+            Serial.print(t);
+            Serial.println(" *C");
+          }      
+        }
       }
     }
   
